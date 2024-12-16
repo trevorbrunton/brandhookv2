0f40dc44d4093f69b69897e3b2813d6fd4726b20
Trevor Brunton <trevor.brunton@woolamaigroup.com>
2024-12-16 20:52:25 +1100

    fixed upload

--- src/app/(pages)/upload/page.tsx
+++ src/app/(pages)/upload/page.tsx
@@ -3,13 +3,17 @@ import { MainContentRow } from "@/components/main-content-row";
 import { PageHeader } from "@/components/page-header";
 import { PageFrame } from "@/components/pageframe";
 import { currentUser } from "@clerk/nextjs/server";
-import {db} from "@/db";
+import { db } from "@/db";
 
 import { NavSideBar } from "@/components/navbars/nav-side-bar";
 import { redirect } from "next/navigation";
 
-export default async function UploadPage({ params }: { params: Promise<{ collectionId: string }> }) {
-  const {collectionId} = (await params);
+export default async function UploadPage({
+  params,
+}: {
+  params: Promise<{ collectionId: string }>;
+}) {
+  const { collectionId } = await params;
   const auth = await currentUser();
 
   const navItems = null;
@@ -37,7 +41,11 @@ export default async function UploadPage({ params }: { params: Promise<{ collect
             <PageHeader title="Upload File" />
             <MainContentRow>
               <div className="flex justify-center w-full  pt-8 min-h-full">
-                <UploadFileForm collectionId={collectionId} userId={user.id} defaultCollectionId={user.defaultCollectionId}/>
+                <UploadFileForm
+                  collectionId={collectionId}
+                  userId={user.id}
+                  defaultCollectionId={user.defaultCollectionId}
+                />
               </div>
             </MainContentRow>
           </div>
@@ -46,5 +54,3 @@ export default async function UploadPage({ params }: { params: Promise<{ collect
     </div>
   );
 }
-
-
