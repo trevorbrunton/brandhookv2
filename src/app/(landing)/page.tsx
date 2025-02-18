3c65ae5807ef52f26cdab29a898adfa5ebe9ae0b
Trevor Brunton <trevor.brunton@woolamaigroup.com>
2025-02-18 15:31:49 +1100

    added video button on landing page

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,6 +3,9 @@ import { ShinyButton } from "@/components/shiny-button";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Image from "next/image";
+import Link from "next/link";
+import {cn} from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 
 
 
@@ -18,38 +21,49 @@ export default async function LandingPage() {
       {/* Hero section */}
       <section className="relative sm:py-24 py-6">
         <MaxWidthWrapper className="text-center bg-white">
-         
-            <div className="flex flex-col items-center h-full space-y-16 ">
-              <div className="flex items-center space-x-4 w-96">
-                <Image
-                  src={"/ladyVC.jpg"}
-                  alt="logo"
-                  width={150}
-                  height={150}
-                  className="rounded-full"
-                />
-                <Image
-                  src="/brandhook.png"
-                  alt="Brandhook"
-                  width={200}
-                  height={100}
-                />
-              </div>
-
-              <span className=" text-wrap font-light text-center text-xl tracking-wide text-normal">
-                Take your brand further.
-              </span>
-
-              <div className="w-full max-w-80">
-                <ShinyButton
-                  href="/sign-up"
-                  className="relative z-10 h-14 w-full text-base shadow-lg transition-shadow duration-300 hover:shadow-xl"
-                >
-                  Start For Free Today
-                </ShinyButton>
-              </div>
+          <div className="flex flex-col items-center h-full space-y-16 ">
+            <div className="flex items-center space-x-4 w-96">
+              <Image
+                src={"/ladyVC.jpg"}
+                alt="logo"
+                width={150}
+                height={150}
+                className="rounded-full"
+              />
+              <Image
+                src="/brandhook.png"
+                alt="Brandhook"
+                width={200}
+                height={100}
+              />
             </div>
-        
+
+            <span className=" text-wrap font-light text-center text-xl tracking-wide text-normal">
+              Take your brand further.
+            </span>
+
+            <div className="w-full max-w-80">
+              <ShinyButton
+                href="/sign-up"
+                className="relative z-10 h-14 w-full text-base shadow-lg transition-shadow duration-300 hover:shadow-xl"
+              >
+                Start For Free Today
+              </ShinyButton>
+            </div>
+
+            <div
+              className={cn(
+                buttonVariants({ variant: "outline" }),
+                "w-30 justify-center flex items-center gap-x-2.5 rounded-md p-6 text-xl font-medium leading-6 text-white hover:bg-gray-50 transition mx-2 mt-20 bg-brown"
+              )}
+            >
+              <Link href="https://cronicle-file-uploads.s3.ap-southeast-2.amazonaws.com/BrandHook+Discover+AI+App+How+to+Use.mov">
+                <span className="">
+                  Watch the Brandhook Discover AI Tool Explainer
+                </span>
+              </Link>
+            </div>
+          </div>
         </MaxWidthWrapper>
       </section>
     </>
